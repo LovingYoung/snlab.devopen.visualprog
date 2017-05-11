8f782a1981889fa2d9b7ab4ebfb371b830293a41
Jace Liu <ly950710@gmail.com>
2017-05-11 18:56:52 +0800

    Add a new block: onPacket
    Signed-off-by: Jace Liu <ly950710@gmail.com>

--- utils/load_blocks.js
+++ utils/load_blocks.js
@@ -2,29 +2,55 @@
  * Created by jaceliu on 09/05/2017.
  */
 
-function string_length(){
-    return {
-        init: function() {
-            this.appendValueInput('VALUE')
-                .setCheck('String')
-                .appendField('length of');
-            this.setOutput(true, 'Number');
-            this.setColour(160);
-            this.setTooltip('Returns number of letters in the provided text.');
-            this.setHelpUrl('http://www.w3schools.com/jsref/jsref_length_string.asp');
+function BlockCollections(){
+    this.block_list = [];
+    this.load_blocks = function () {
+        for(var i = 0; i < this.block_list.length; i++){
+            var name = this.block_list[i];
+            var fn = this[name];
+            if(typeof fn === 'function')
+                Blockly.Blocks[name] = fn();
         }
-    };
+    }
 }
 
-var block_list = [
-    "string_length"
+var functions = new BlockCollections();
+var constants = new BlockCollections();
+var variables = new BlockCollections();
+
+/* Functions */
+functions.onPacket = function(){
+    var json = {
+        "message0": "onPacket with pkt",
+        "args0": [],
+        "message1": "Do %1",
+        "args1": [{type: "input_statement", "name": "DO"}],
+        "message2": "set action %1",
+        "args2": [{type: "input_value", "name":"ACTION", "check": "Number", "align": "RIGHT"}],
+        "colour": 100
+    };
+    return {
+        init: function(){
+            this.jsonInit(json);
+            this.setDeletable(false);
+        }
+    }
+};
+
+functions.block_list = ["onPacket"];
+
+/* Constants */
+
+/* Variables */
+
+var collection_list= [
+    'functions',
+    'constants',
+    'variables'
 ];
 
 function load_blocks(){
-    for(var i = 0; i < block_list.length; i++){
-        var name = block_list[i];
-        var fn = window[name];
-        if(typeof fn === 'function')
-            Blockly.Blocks[name] = fn();
+    for(var i = 0; i < collection_list.length; i++){
+        window[collection_list[i]].load_blocks();
     }
 }
