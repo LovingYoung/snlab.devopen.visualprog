089f6b5bedb8f20255379d7a1464fbc5d60568c7
Jace Liu <ly950710@gmail.com>
2017-05-11 19:21:30 +0800

    Add 3 block types: IPv4SrcIs, IPv4DstIs, static_ip
    Signed-off-by: Jace Liu <ly950710@gmail.com>

--- utils/load_blocks.js
+++ utils/load_blocks.js
@@ -24,9 +24,9 @@ functions.onPacket = function(){
         "message0": "onPacket with pkt",
         "args0": [],
         "message1": "Do %1",
-        "args1": [{type: "input_statement", "name": "DO"}],
+        "args1": [{"type": "input_statement", "name": "DO"}],
         "message2": "set action %1",
-        "args2": [{type: "input_value", "name":"ACTION", "check": "Number", "align": "RIGHT"}],
+        "args2": [{"type": "input_value", "name":"ACTION", "check": "Number", "align": "RIGHT"}],
         "colour": 100
     };
     return {
@@ -37,9 +37,59 @@ functions.onPacket = function(){
     }
 };
 
-functions.block_list = ["onPacket"];
+functions.IPv4SrcIs = function() {
+    var json = {
+        "message0": "IPv4SrcIs %1",
+        "args0": [{"type": "input_value", "name": "IP", "check": "IP"}],
+        "output": "Boolean",
+        "colour": 100
+    };
+    return {
+        init: function () {
+            this.jsonInit(json);
+        }
+    }
+};
+
+functions.IPv4DstIs = function () {
+    var json = {
+        "message0": "IPv4DstIs %1",
+        "args0": [{"type": "input_value", "name": "IP", "check": "IP"}],
+        "output": "Boolean",
+        "colour": 100
+    };
+    return {
+        init: function () {
+            this.jsonInit(json);
+        }
+    }
+};
+
+functions.block_list = ["onPacket", "IPv4SrcIs", "IPv4DstIs"];
 
 /* Constants */
+
+constants.static_ip = function () {
+    var json = {
+        "message0": "%1.%2.%3.%4",
+        "args0":[
+            {"type": "field_number", "name": "IP_1", "check": "Number"},
+            {"type": "field_number", "name": "IP_2", "check": "Number"},
+            {"type": "field_number", "name": "IP_3", "check": "Number"},
+            {"type": "field_number", "name": "IP_4", "check": "Number"}
+        ],
+        "output": "IP",
+        "colour": "150",
+        "inputsInline": true
+    };
+    return {
+        init: function () {
+            this.jsonInit(json);
+        }
+    }
+};
+
+constants.block_list = ['static_ip'];
 
 /* Variables */
 
