82842ccb1ab51e66658b7f47dd5f3c9940ee500d
Jace Liu <ly950710@gmail.com>
2017-05-15 20:38:40 +0800

    Update variable utilities
    Signed-off-by: Jace Liu <ly950710@gmail.com>

--- utils/variables.js
+++ utils/variables.js
@@ -3,13 +3,34 @@
  */
 
 function getTypeAndName(varName) {
-    if(!varName.includes('-') && !varName.includes('_')) return ['int', varName];
+
+  var isNumber = function (varName) {
+    var digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.']
+    for(var i = 0; i < varName.length; i++){
+      if(! varName[i] in digits) return false;
+    }
+    return true;
+  };
+
+  var isDouble = function (varName) {
+    return varName.includes('.');
+  };
+
+  if(!varName.includes('-') && !varName.includes('_')){
+    if(isNumber(varName)){
+      if(isDouble(varName)) return ['double', varName];
+      else return ['int', varName];
+    } else {
+      return ['String', varName];
+    }
+  } else {
     var varType;
-    if(varName.includes('-')) {
-        varType = varName.split('-')[0];
-    } else{
-        varType = varName.split('_')[0];
+    if (varName.includes('-')) {
+      varType = varName.split('-')[0];
+    } else {
+      varType = varName.split('_')[0];
     }
     varName = varName.slice(varType.length + 1);
     return [varType, varName];
+  }
 }
